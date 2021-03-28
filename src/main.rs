7d6204534761a9b070cc9a0bf8fa3e8888e853bc
padrition <padrition@gmail.com>
2021-03-28 21:47:29 +0600

    added a search_case_insensetive variable to perform case insensetive search. Added environment variable for seting program to case insensetive mode

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,14 @@ use std::process;
 use lib::Config;
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem parsing arguments: {}",err);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for : {}", config.query);
-    println!("In file :{}", config.filename);
-
-    if let Err(e) = lib::run(config){
+    if let Err(e) = lib::run(config) {
         println!("Application error : {}", e);
 
         process::exit(1);

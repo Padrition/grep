8532c092d94f5684d3d3be8a957a29d7eaa18a56
padrition <padrition@gmail.com>
2021-03-29 23:11:07 +0600

    changed searches to use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,18 @@ pub struct Config {
     pub case_sensetive: bool,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next(){
+            Some(arg) => arg,
+            None => return Err("No query was specified!"),
+        };
+
+        let filename = match args.next(){
+            Some(arg) => arg,
+            None => return Err("No file name was specified!"),  
+        };
 
         let case_sensetive = env::var("CASE_SENSETIVE").is_err();
 
@@ -40,26 +46,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 fn search_case_insensetive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 #[cfg(test)]
 mod tests {
